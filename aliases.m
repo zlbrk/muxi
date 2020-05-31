1d47e9ecf9f81b10caf0fd625c283961f7310122
Nikita Pristupchik <nikita.pristupchik@gmail.com>
2020-05-31 11:42:04 +0300

    Определено полное и сокращённое имя функции удаления выбранных точек

--- aliases.m
+++ aliases.m
@@ -3,12 +3,14 @@
 refreshView = @() refreshView();
 pp = @(Ids) pickPoints(Ids);
 dp = @(Ids) dropPoints(Ids);
+delp = @() deletePoints(); % команда специально длиннее, чтобы нельзя было по ошибке её ввести
 an = @(num) addPointsNext(num);
 cc = @() clearCart();
 mv = @(dz, dr) movePoints(dz, dr);
 tm = @() toggleMesh();
 sv = @(V) setVoltage(V);
 cf = @() createFile();
+
 
 initFigure = @(ZMIN, ZMAX, RMIN, RMAX) initFigure(ZMIN, ZMAX, RMIN, RMAX);
 drawVoid = @() drawVoid();
@@ -17,3 +19,5 @@ drawCathode = @() drawCathode();
 showPointIDs = @() showPointIDs();
 
 % save('aliases.mat'); % на всякий случай?
+% Проверка
+% BRK = @() setBreakPoint();
