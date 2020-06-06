e6a8d6d61c75e187011299a05ec1a30b4f6c10a7
Nikita Pristupchik <nikita.pristupchik@gmail.com>
2020-06-06 09:15:46 +0300

    deletePoints function implemented.
    E.g. we can do it more intellectual replacing pointwise elimination by groupwise one but at the same time, we need to find groups of neighboring points in the sorted cart somehow. Next, we could want to make a groupwise elimination concurrent, which means we need to split the initial structure by independent parts, so we can delete selected points in all chunks safely at once. Suggestions are welcomed.

--- ux/deletePoints.m
+++ ux/deletePoints.m
@@ -8,86 +8,25 @@ function deletePoints()
 		fprintf('At least one point should be selected!\n');
 		return
 	else
-		sortedCart = sort(Cart);
-		nearestIds = find(diff(sortedCart)==1)
-		% нужно найти группы ближайших идентификаторов
-	end
-
-% Если выбирается отрицательное число новых точек контура
-	if number < 1
-		fprintf ('Enter a positive number!\n');
-		return
-	end
-
-% Если выбирается последняя точка контура
-numPoints = [ConStruct.id];
-
-	if Cart == max(numPoints)
-		fprintf('Last point selected!\n');
-		return
-	end
-
-	PointId = Cart;
-
-	ConStructHead = ConStruct(1:PointId);
-	ConStructTail = ConStruct(PointId+1:end);
-
-    for i = 1:numel(ConStructTail)
-		%Сдвигаем индексы хвостовой части на количество добавляемых точек
-		ConStructTail(i).id = ConStructTail(i).id+number;
-    end
-    % NewPoints = [];
-	NewPoints = evalNewPointsData(ConStructHead(end), ConStructTail(1), number);
-
-	ConStruct = [ConStructHead NewPoints ConStructTail];
-
-	refreshView(); % Обновляем вид
-end
-
-function NewPoints = evalNewPointsData(HeadLast, TailFirst, number)
-	% global ConStruct;
-
-	% Здесь фокус в том, чтобы не забыть начальную и конечную точки при использовании linspace
-	if HeadLast.Z~=TailFirst.Z
-		tempZR(:, 1) = linspace(HeadLast.Z, TailFirst.Z, number+2);
-		newZR(:, 1) = tempZR(2:end-1, 1);
-	else
-		newZR(:, 1) = HeadLast.Z*ones(number, 1);
-	end
-
-	if HeadLast.R~=TailFirst.R
-		tempZR(:, 2) = linspace(HeadLast.R, TailFirst.R, number+2);
-		newZR(:, 2) = tempZR(2:end-1, 2);
-	else
-		newZR(:, 2) = HeadLast.R*ones(number, 1);
-	end
-
-	if HeadLast.Voltage~=TailFirst.Voltage
-		tempVoltage = linspace(HeadLast.Voltage, TailFirst.Voltage, number+2);
-		newVoltage = tempVoltage(2:end-1, 2);
-		tempPhi = linspace(HeadLast.Phi, TailFirst.Phi, number+2);
-		newPhi(:, 2) = tempPhi(2:end-1, 2);
-	else
-		newVoltage = HeadLast.Voltage*ones(number, 1);
-		newPhi = HeadLast.Phi*ones(number, 1);
-	end
-
-	NewPoints = [];
-	for i = 1:number
-		NPoint.id = HeadLast.id + i;
-		NPoint.Voltage = newVoltage(i);
-		NPoint.Phi = newPhi(i);
+		sortedCart = sort(Cart,'descend'); % Выбранные точки отсортированы в нисходящем порядке
 		
-		NPoint.prev = NPoint.id - 1;
-		% новые точки не могут выйти за границы HeadLast-TailFirst
+		for i = sortedCart
+			delPoint(i);
+		end
+	end
+	clearCart();
+	refreshView(); % Обновляем вид
 
-		NPoint.next = NPoint.id + 1;
+function delPoint(id)
 
-		NPoint.Z = newZR(i, 1);
+	Head = ConStruct(1:id-1); % Точки от начала структуры до удаляемой точки, исключая её саму.
+	Tail = ConStruct(id+1:end); % Точки от удаляемой, не включая её, до конца структуры
 	
-		NPoint.R = newZR(i, 2);
-
-		NewPoints = [NewPoints NPoint];
+	for j = 1:numel(Tail)
+		Tail(j).id = Tail(j).id-1; % сдвигаем индексы хвостовых точек на один шаг назад
 	end
 
+	ConStruct = [Head Tail]; % Новая структура, не содержащая исключаемую точку.
+
+end
 end
