055144eb23495b3d1e20774c8e76078066d2a8d2
Nikita Pristupchik <nikita.pristupchik@gmail.com>
2020-06-07 10:39:16 +0300

    Done it!

--- main.m
+++ main.m
@@ -6,9 +6,21 @@ clc;
 %Определение псевдонимов API
 aliases;
 
-
+% Инициализация файловой системы проекта
 opath = path; % old path
-path(opath,'ux; render; bin; io; TEST/IN/'); % new path
+workdir = pwd;
+muxiFS = sprintf('%s; ux; render; bin; io; TEST/IN;', workdir);
+% разделяем строку с описанием файловой системы на отдельные поддиректории
+subdirs = split(muxiFS); 
+% создаём поддиректории, если они не существуют (если нет дескрипторов типа folder (7) с именем поддиректории)
+for i=2:numel(subdirs)
+	subdir = cell2mat(subdirs(i));
+	if exist(subdir(1:end-1),'dir') ~= 7
+		% subdir(1:end-1) % вывод имени поддиректории для отладки
+		mkdir(subdir(1:end-1));
+	end
+end
+path(opath, muxiFS); % new path
 clear opath;
 
 global Colors;
