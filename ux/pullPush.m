d79096ad5f57882f0353f05e452a66ccb09f7d4d
Nikita Pristupchik <nikita.pristupchik@gmail.com>
2020-08-23 20:41:03 +0300

    Pull operation can be done if both neighboring segments lay in line with picked one

--- ux/pullPush.m
+++ ux/pullPush.m
@@ -1,6 +1,7 @@
 % Основной инструмент прямого редактирования контура 
 function pullPush(off_mm)
 	global SCart; % Нам понадобятся выбранные стороны
+	% global PCart; % Нам понадобится корзинка с точками стороны
 	global ConStruct; % И координаты точек
 
 	if isempty(SCart)
@@ -13,14 +14,27 @@ function pullPush(off_mm)
 	if numel(SCart)==1
 		fprintf('Side #%d pulled by %g mm!\n', SCart, off_mm);
 	end
-	pointsToFork = whichPointsToFork(SCart);
+	[pointsToFork, directions] = whichPointsToFork(SCart)
+	sortedPointsToFork = sort(pointsToFork,'ascend');
+	if numel(sortedPointsToFork)==2
+		clearCarts();
+		pickPoints(sortedPointsToFork(1));
+		forkForward(0); % Дублируем первую точку
+		clearCarts();
+		pickPoints(sortedPointsToFork(2)+1);
+		forkBackward(0); % Дублируем вторую точку, которая остаётся выбранной
+		clearCarts();
+		pickSides(sortedPointsToFork(1)+1); % Выбираем новую сторону
+		PD = off_mm*directions(:,1); % Points displacement
+		movePointsZR(PD(1),PD(2));
+	end
 
 	setCheckpoint(); % Создаём чекпойнт
 	refreshView(); % Обновляем вид
 end
 
 %% whichNeedFork: function description
-function [pointsToFork] = whichPointsToFork(sNumber)
+function [pointsToFork, directions] = whichPointsToFork(sNumber)
 	global Sides;
 	C = Sides(sNumber).id; % номер выбранной стороны
 	P = Sides(sNumber).prev; % номер предыдущей стороны
@@ -30,14 +44,34 @@ function [pointsToFork] = whichPointsToFork(sNumber)
 	PN = getNormal(P) % вектор нормали предыдущей стороны
 	NN = getNormal(N) % вектор нормали следующей стороны
 
-	PC_cross = cross([CN 0], [PN 0]); % prev - central normals cross product
+	PC_cross = cross([CN' 0], [PN' 0]); % prev - central normals cross product
 	PC_mod = PC_cross*PC_cross'; % prev - central cross product modulus
-	NC_cross = cross([CN 0], [NN 0]); % next - central normals cross product
+	NC_cross = cross([CN' 0], [NN' 0]); % next - central normals cross product
 	NC_mod = NC_cross*NC_cross'; % next - central cross product modulus
-	pointsToFork = [];
+
+	if (PC_mod <= eps) && (NC_mod <= eps) % Все три сегмента лежат на одной прямой
+		pointsToFork = [Sides(sNumber).SP Sides(sNumber).EP];
+		directions = [CN CN];
+	elseif (PC_mod <= eps) && (NC_mod > eps) % Левый сегмент лежит, а правый нет
+		pointsToFork = [Sides(sNumber).SP];
+		NPD = rotateVector(getTangential(N), 180); % Next point direction
+		directions = [CN NPD];
+	elseif (PC_mod > eps) && (NC_mod <= eps) % Правый лежит, а левый - нет
+		pointsToFork = [Sides(sNumber).EP];
+		PPD = getTangential(P); % Prev point direction
+		directions = [PPD CN];
+	end
 end
 
 function [normal] = getNormal(sNumber)
+	global Sides;
+	global ConStruct;
+	[ntnd] = getTangential(sNumber); % normalized tangential
+	[normal] = rotateVector(ntnd, 90); % normalized normal
+end
+
+%% getTangential: function description
+function [ntnd] = getTangential(sNumber)
 	global Sides;
 	global ConStruct;
 	% start point ZR
@@ -46,10 +80,15 @@ function [normal] = getNormal(sNumber)
 	% end point ZR
 	EPZ = ConStruct(Sides(sNumber).EP).Z;
 	EPR = ConStruct(Sides(sNumber).EP).R;
-	tng = [(EPZ-SPZ) (EPR - SPR)]; % tangential={dz, dr}
-	ntng = tng./sqrt(tng*tng'); % normalization
-	RM = [cosd(90) sind(90)
-				-sind(90) cosd(90)]; % Rotation matrix
-	normal = RM*ntng';
-
+	tng = [(EPZ-SPZ) (EPR - SPR)]'; % tangential={dz, dr}
+	ntnd = tng./sqrt(tng'*tng); % normalization
 end
+
+%% rotateVector: function description
+function [vec1] = rotateVector(vec0, deg)
+	RM = [cosd(deg) sind(deg)
+				-sind(deg) cosd(deg)]; % Rotation matrix
+	vec1 = RM*vec0;
+end
+
+	
