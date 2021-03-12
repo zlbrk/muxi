07ccf36b8bc74b99e1ef913512340cf21463153e
Nikita Pristupchik <nikita.pristupchik@gmail.com>
2021-03-12 15:52:33 +0300

    ZONES global variable introduced

--- ux/display/listLabels.m
+++ ux/display/listLabels.m
@@ -1,11 +1,11 @@
-function kLT = listLabels()
+function varargout = listLabels()
 global LABELS;
 
 % выводим имена существующих меток
-fprintf('\nKnown labels are:\n') % display known labels
 for i = 1:numel(LABELS)
 	kL{i,1} = LABELS(i).id; % display name of the known label
 	str = sprintf('%s ',string(LABELS(i).sides));
 	kL{i,2} = str(1:end-1); % sides, which this label assigned to
 end
-kLT = cell2table(kL, 'VariableNames', {'label' 'sides'});
+varargout{1} = cell2table(kL, 'VariableNames', {'label' 'sides'})
+fprintf('\nKnown labels stored in  table!\n') % display known labels
