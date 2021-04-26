89443bc26bb99bf1832d344d77e00c4d7282919e
Nikita Pristupchik <nikita.pristupchik@gmail.com>
2021-04-26 21:37:53 +0300

    setCATS function call computes CATS.Zs correctly

--- main.m
+++ main.m
@@ -15,23 +15,23 @@ initGlobals;
 initFigure();
 
 % Определяем параметры катода
-CATS.Zc = 2;
-CATS.Rc = 4.3;
-CATS.Rs = 9;
-CATS.Zs = 10;
-CATS.nTR = 30;
-CATS.SD = 0.075;
+CATS.Zc = 2;    % Z-координата катода (Z2)
+CATS.Rc = 4.3;  % Радиус катода
+CATS.Rs = 9;    % Радиус сферы
+CATS.nTR = 30;  % Количество траекторий
+CATS.SD = 0.075;% Sampling distance
+
+% Инициализируем Пробный регион
+initDummy();
+
+% Доопределяем катод - положение центра сферы должно быть вычислено
+setCATS(CATS.Zc, CATS.Rc, CATS.Rs, CATS.nTR, CATS.SD);
 
 % Определяем параметры зон
 ZONES.Z = [0.9, 2, 4, 8];
 ZONES.R = [0, 2, 4.3];
 ZONES.ZN = [55, 50, 45];
 ZONES.RN = [210, 220];
-
-
-% Инициализируем Пробный регион
-initDummy();
-
 
 %% Инициализация журналирования
 delete 'temp/*' % очистка временных файлов
